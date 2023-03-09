8ebd378010b9a3d1c11a414458c33ef4a1c6037c
YoonSung Kim <opqudk@gmail.com>
2023-03-09 14:58:25 +0900

    style: apply es lint, base url (#18)
    설정한 규격으로 포메팅,
    base url로 import
    
    resolving: #18

--- remote/src/components/Menu.tsx
+++ remote/src/components/Menu.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Maps from './Maps/Maps'
+import { Suspense } from 'react';
+import Maps from 'components/Maps/Maps';
 
 export const Menu = () => {
   // let MapsComponent = React.lazy(() => import('./components/Maps'));
   return (
     <div style={{ backgroundColor: 'green' }}>
-      <React.Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <div> 여기가 Remote </div>
         <Maps />
-      </React.Suspense>
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 export default Menu;
