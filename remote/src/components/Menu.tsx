c5236f5ede7d7e7eb3ddf6ad7925ed76e526ac44
YoonSung Kim <opqudk@gmail.com>
2023-03-09 15:08:24 +0900

    style: test styled component (#18)

--- remote/src/components/Menu.tsx
+++ remote/src/components/Menu.tsx
@@ -1,15 +1,20 @@
 import { Suspense } from 'react';
+import styled from 'styled-components';
+
 import Maps from 'components/Maps/Maps';
 
 export const Menu = () => {
+  const Div = styled.div`
+    background-color: green;
+  `;
   // let MapsComponent = React.lazy(() => import('./components/Maps'));
   return (
-    <div style={{ backgroundColor: 'green' }}>
+    <Div>
       <Suspense fallback={<div>Loading...</div>}>
         <div> 여기가 Remote </div>
         <Maps />
       </Suspense>
-    </div>
+    </Div>
   );
 };
 export default Menu;
