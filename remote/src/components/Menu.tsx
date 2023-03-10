47bce0e41321e3d7f20040f8aba4213de1b00c43
YoonSung Kim <opqudk@gmail.com>
2023-03-10 15:44:34 +0900

    Design: add custom vh, vw (#15)
    calc를 활용해서 스크롤 제거
    
    resolving: #15
    ref: #1, #18

--- remote/src/components/Menu.tsx
+++ remote/src/components/Menu.tsx
@@ -5,13 +5,14 @@ import Maps from 'components/Maps/Maps';
 
 export const Menu = () => {
   const Div = styled.div`
+    height: calc(var(--vh, 1vh) * 100 - 63px);
+    width: calc(var(--vw, 1vw) * 100);
     background-color: green;
   `;
   // let MapsComponent = React.lazy(() => import('./components/Maps'));
   return (
     <Div>
       <Suspense fallback={<div>Loading...</div>}>
-        <div> 여기가 Remote </div>
         <Maps />
       </Suspense>
     </Div>
