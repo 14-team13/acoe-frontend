c987590860776e140b0534c60273e42b844d5ff0
YoonSung Kim <opqudk@gmail.com>
2023-03-15 09:48:49 +0900

    fix: remove unused vars, add type (#27)

--- host/src/pages/LogIn/index.tsx
+++ host/src/pages/LogIn/index.tsx
@@ -1,10 +1,15 @@
 import useInput from '@hooks/useInput';
-import { Button, Error, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/styles';
-import fetcher from '@utils/fetcher';
-import axios from 'axios';
-import React, { useCallback, useState } from 'react';
-import useSWR from 'swr';
-import { login } from "@api/main"
+import {
+  Button,
+  Error,
+  Form,
+  Header,
+  Input,
+  Label,
+  LinkContainer,
+} from '@pages/SignUp/styles';
+import { useCallback, useState } from 'react';
+import { login } from '@api/main';
 
 const LogIn = () => {
   // const { data: userData, error, mutate } = useSWR('/api/users', fetcher);
@@ -12,39 +17,54 @@ const LogIn = () => {
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
 
-  const onSubmit = useCallback(async (e) => {
-    e.preventDefault();
-    setLogInError(false);
-    const response = await login(email, password)
-    if (response.status === 200) {
-      window.open('/', '_self')
-    }
-  },
-    [email, password],
+  const onSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setLogInError(false);
+      const response = await login(email, password);
+      if (response.status === 200) {
+        window.open('/', '_self');
+      }
+    },
+    [email, password]
   );
 
   return (
-    <div id="container">
+    <div id='container'>
       <Header>ACOE</Header>
       <Form onSubmit={onSubmit}>
-        <Label id="email-label">
+        <Label id='email-label'>
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+            <Input
+              type='email'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChangeEmail}
+            />
           </div>
         </Label>
-        <Label id="password-label">
+        <Label id='password-label'>
           <span>비밀번호</span>
           <div>
-            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+            <Input
+              type='password'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChangePassword}
+            />
           </div>
-          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+          {logInError && (
+            <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>
+          )}
         </Label>
-        <Button type="submit">로그인</Button>
+        <Button type='submit'>로그인</Button>
       </Form>
       <LinkContainer>
         아직 회원이 아니신가요?&nbsp;
-        <a href="/signup">회원가입 하러가기</a>
+        <a href='/signup'>회원가입 하러가기</a>
       </LinkContainer>
     </div>
   );
