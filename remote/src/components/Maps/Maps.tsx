24202fcb32a84b958ee4962f136f2227ab04b2d8
Youjeong Park <pyj0695@gmail.com>
2023-03-26 21:37:31 +0900

    add markers in map(#37) resolving : #37

--- remote/src/components/Maps/Maps.tsx
+++ remote/src/components/Maps/Maps.tsx
@@ -5,7 +5,11 @@ import { Map, View } from 'ol';
 import { fromLonLat, get as getProjection } from 'ol/proj';
 import { XYZ } from "ol/source";
 import { Tile as TileLayer } from "ol/layer";
-
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { Icon, Style, Circle as CircleStyle, Fill, Stroke } from "ol/style";
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
 
 const MapWrap = styled.div`
   width: 100%;
@@ -32,6 +36,36 @@ const Maps = () => {
       target: mapRef.current
     });
 
+    const vectorSource = new VectorSource();
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+    mapObj.addLayer(vectorLayer);
+
+    // Add markers to the vector layer
+    const newMarkers = [
+      { lon: 126.92054263154023, lat: 37.56311839665308, name: 'Marker 1' },
+      { lon: 127.92054263154023, lat: 36, name: 'Marker 2' },
+      { lon: -0.118092, lat: 51.509865, name: 'Marker 3' },
+    ];
+
+    const markerStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 1],
+        src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+      }),
+    });
+
+    const newFeatures = newMarkers.map((marker) => {
+      const point = new Point(fromLonLat([marker.lon, marker.lat]));
+      const feature = new Feature({
+        geometry: point,
+        name: marker.name,
+      });
+      feature.setStyle(markerStyle);
+      return feature;
+    });
+    vectorSource.addFeatures(newFeatures);
     mapObj.setTarget(mapRef.current);
 
     return () => mapObj.setTarget('');
