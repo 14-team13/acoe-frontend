acbe74479111738a09bd410f894651f194f98e8e
Youjeong Park <pyj0695@gmail.com>
2023-03-26 21:18:57 +0900

    feat : add 연희동 latitude, longtitude (#37)
    resolving : #37

--- remote/src/components/Maps/Maps.tsx
+++ remote/src/components/Maps/Maps.tsx
@@ -1,9 +1,11 @@
 import 'ol/ol.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef, ReactNode } from 'react';
 import styled from 'styled-components';
 import { Map, View } from 'ol';
-import { Tile } from 'ol/layer';
-import { OSM } from 'ol/source';
+import { fromLonLat, get as getProjection } from 'ol/proj';
+import { XYZ } from "ol/source";
+import { Tile as TileLayer } from "ol/layer";
+
 
 const MapWrap = styled.div`
   width: 100%;
@@ -19,10 +21,15 @@ const Maps = () => {
 
     const mapObj = new Map({
       view: new View({
-        center: [-11000000, 4600000],
-        zoom: 2,
-      }),
-      layers: [new Tile({ source: new OSM() })],
+        center: fromLonLat([126.92054263154023, 37.56311839665308]),
+        zoom: 16,
+      }), 
+      layers: [
+        new TileLayer({
+          source: new XYZ({ url: "http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png" }),
+        }),
+      ],
+      target: mapRef.current
     });
 
     mapObj.setTarget(mapRef.current);
