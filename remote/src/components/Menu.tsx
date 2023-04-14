7480fdc90a2ea05ebd2e1e07077533870489f132
YoonSung Kim <opqudk@gmail.com>
2023-04-14 22:56:05 +0900

    Design: add left modal (#36)

--- remote/src/components/Menu.tsx
+++ remote/src/components/Menu.tsx
@@ -1,24 +1,31 @@
 import styled from 'styled-components';
 
 import { Maps } from 'components/Maps';
-import { Header, MixedBoundary } from 'components/Common';
+import {
+  // Header,
+  MixedBoundary,
+} from 'components/Common';
 import ModalContainer from 'components/LeftModal/ModalContainer';
 
 const Container = styled.section`
   display: flex;
   flex-direction: column;
   position: relative;
+  height: calc(var(--vh, 1vh) * 100);
+  width: calc(var(--vw, 1vw) * 100);
 `;
 
 const Wrap = styled.div`
-  height: calc(var(--vh, 1vh) * 100 - 40px);
-  width: calc(var(--vw, 1vw) * 100);
+  /* height: calc(var(--vh, 1vh) * 100);
+  width: calc(var(--vw, 1vw) * 100); */
+  height: 100%;
+  widht: 100%;
 `;
 
 export const Menu = () => {
   return (
     <Container>
-      <Header />
+      {/* <Header /> */}
       <ModalContainer />
       <Wrap>
         <MixedBoundary>
--- remote/src/styles/common.ts
+++ remote/src/styles/common.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+import { theme } from './theme';
+
+const { whiteButton, button, onWhiteButton, onButton } = theme;
+
 export const errorLoadingWrap = styled.div`
   background: #fff;
   margin-bottom: 8px;
@@ -9,4 +13,20 @@ export const errorLoadingWrap = styled.div`
   div:after {
     background: #383f5a;
   }
+`;
+
+export const Button = styled.div<{ on?: unknown }>`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 12px 16px;
+  min-width: 50px;
+  height: 54px;
+  border-radius: 100px;
+  font-weight: 700;
+  font-size: 20;
+  line-height: 30px;
+  letter-spacing: -0.02em;
+  background-color: ${({ on }) => (on === null ? whiteButton : button)};
+  color: ${({ on }) => (on === null ? onWhiteButton : onButton)};
 `;
