cd605c177335b377c5fe2e00ce48eeb9a780adc7
Youjeong Park <pyj0695@gmail.com>
2023-04-27 22:39:38 +0900

    design : blog bottom lines

--- remote/src/components/BlogReviewComponent.jsx
+++ remote/src/components/BlogReviewComponent.jsx
@@ -1,7 +1,7 @@
 const BlogReviewComponent = (props) => {
 
   return (
-    <div className = "cafe-blog">
+    <div className = "cafe-blog mgt24">
       <div className = "fw400 fs12 lh18 fc-gray mgb8">이로운 세무회계 컨설팅</div>
       <div className = "fw700 fs16 lh24 mgb8">연희동 비건 카페/제로웨이스트 카페 반려동물 동반 가능 보틀라운지</div>
       <div className = "fw400 fs14 lh21 mgb4">일회용품을 전혀 사용하지 않는 제로웨이스트 카페, 평소에 무척 궁금했던 보틀라운지에 갔다. 항상 사람도 많고 밖에...</div>
