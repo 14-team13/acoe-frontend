c4384ab1e7d055e00b573565484d2e7e8974a669
Youjeong Park <pyj0695@gmail.com>
2023-05-12 21:49:18 +0900

    fix : change original tab data

--- host/src/layouts/AcoeAdmin.tsx
+++ host/src/layouts/AcoeAdmin.tsx
@@ -1,24 +1,33 @@
 import { Tabs, Tab } from 'react-bootstrap'
+import { useState} from 'react';
 import MembersManagement from '@pages/components/MembersManagement'
 import FranchiseManagement from '@pages/components/FranchiseManagement'
 import CafesManagement from '@pages/components/CafesManagement'
 
 
 const AcoeAdmin = () => {
+
+  const [key, setKey] = useState('')
+
+  const handleSelect = (key) => {
+    setKey(key)
+  }
+
   return (
     <Tabs
+      onSelect={handleSelect}
       defaultActiveKey="member"
       id="uncontrolled-tab-example"
       className="mb-3 mgt10 mg20"
     >
       <Tab eventKey="member" title="회원관리">
-        <MembersManagement />
+        <MembersManagement key = {key}/>
       </Tab>
       <Tab eventKey="franchise" title="프렌차이즈 관리">
-        <FranchiseManagement />
+        <FranchiseManagement key = {key}/>
       </Tab>
       <Tab eventKey="cafe" title="카페 관리">
-        <CafesManagement />
+        <CafesManagement key = {key}/>
       </Tab>
     </Tabs>
   );
