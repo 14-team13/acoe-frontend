f4fd02406408be94df8ba141a92a75e4e70ae188
Youjeong Park <pyj0695@gmail.com>
2023-05-20 08:54:09 +0900

    feat : proxy server

--- remote/src/api/core/index.tsx
+++ remote/src/api/core/index.tsx
@@ -12,7 +12,6 @@ const request = axios.create({
     'Content-Type': 'application/json',
     // "Access-Control-Allow-Origin": `http://acoe.co.kr`,
     // 'Access-Control-Allow-Credentials':"true",
-    'x-cors-api-key': process.env.REACT_APP_CORS
   }
 });
 
