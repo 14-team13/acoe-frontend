83eb8bbe0a90335e8fdac24d3a84898ca1aa5da2
Youjeong Park <pyj0695@gmail.com>
2023-05-24 18:18:18 +0900

    Revert "feat : credential 주석"
    This reverts commit cd0f6b72495df6e695607501923740863dd68abb.

--- host/src/api/core/index.tsx
+++ host/src/api/core/index.tsx
@@ -18,7 +18,7 @@ const request = axios.create({
 
 //요청 타임아웃 설정
 request.defaults.timeout = 2500;
-// request.defaults.withCredentials = true;
+request.defaults.withCredentials = true;
 // request.defaults.
 // request.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
@@ -82,7 +82,7 @@ export const Axios = () => {
       
 
       session.defaults.timeout = 2500;
-      // session.defaults.withCredentials = true;
+      session.defaults.withCredentials = true;
 
       session.interceptors.request.use(
         async (config: InternalAxiosRequestConfig<any>) => {
