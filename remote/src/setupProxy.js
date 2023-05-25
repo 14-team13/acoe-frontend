98c159e5b327ce136ff96ef700b78f8ad82fb1d6
Youjeong Park <pyj0695@gmail.com>
2023-05-25 09:04:21 +0900

    fix : development setting proxy

--- remote/src/setupProxy.js
+++ remote/src/setupProxy.js
@@ -3,19 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use(
     createProxyMiddleware('/main', {
-      target: 'https://acoe.co.kr/',
+      target: 'https://acoe.co.kr/api/',
       changeOrigin: true,
     }),
   ),
     app.use(
       createProxyMiddleware('/admin', {
-        target: 'https://acoe.co.kr/',
+        target: 'https://acoe.co.kr/api/',
         changeOrigin: true,
       }),
     ),
     app.use(
       createProxyMiddleware('/api', {
-        target: 'https://acoe.co.kr/',
+        target: 'https://acoe.co.kr/api/',
         changeOrigin: true,
       }),
     )
